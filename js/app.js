bcd899901ece6b1283d5bfb3f599017dbc10c621
The Do <dqthebt@gmail.com>
2019-05-23 07:17:03 +0700

    HM: commit add controller pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
+// $(document).ready(function(){
+//     $('.header').height($(window).height());
+// })
 
-var appForecast = angular.module("appForecast",["ngRoute", "ngResource"]);
+var appForecast = angular.module('appForecast',['ngRoute', 'ngResource']);
+
+appForecast.config(['$routeProvider', function($routeProvider){
+	
+	$routeProvider.when('#!/',{
+		templateUrl: '../pages/home.html',
+		controller: 'homepageController'
+	})
+
+	. when('#!/forecast',{
+		templateUrl: '../pages/forecast.html',
+		controller: 'forecastController'
+	});
+}]);
+
+appForecast.controller('homepageController',['$scope', function($scope){
+
+}]);
+
+appForecast.controller('forecastController', ['$scope', function($scope){
+
+}]);
